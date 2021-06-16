8de1ecb1bb307d89538ff2ddf76062854d5c4395
jiwoo-coding <84822865+jiwoo-coding@users.noreply.github.com>
2021-06-16 10:00:07 +0900

    Update 2644.py

--- Baek_Joon/2644.py
+++ Baek_Joon/2644.py
@@ -11,24 +11,24 @@ from collections import deque
 num=int(input())
 a, b=map(int, input().split())
 m=int(input())
-relation=[[] for i in range(n+1)]
-ch = [0]*(n+1)
+relation=[[] for i in range(num+1)]
+ch = [0]*(num+1)
 q=deque()
 
 for i in range(m):
-  x,y=map(int, input().split())
-  relation[x].append(y)
-  relation[y].append(x)
-  
- q.append(a)
+    x,y=map(int, input().split())
+    relation[x].append(y)
+    relation[y].append(x)
+
+    q.append(a)
 
 while q:
-  n=q.popleft()
-  for i in relation[n]:
-    if i != a and ch[i]==0:
-      ch[i]=ch[n]+1
-      q.append(i)
+    n=q.popleft()
+    for i in relation[n]:
+        if i != a and ch[i]==0:
+            ch[i]=ch[n]+1
+            q.append(i)
 if ch[b]==0:
-  print(-1)
+    print(-1)
 else:
-  print(ch[b])
+    print(ch[b])
