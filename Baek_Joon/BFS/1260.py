38b365ae5054b71d614a8c57633f239e0562ef75
jiwoo-coding <84822865+jiwoo-coding@users.noreply.github.com>
2021-07-06 00:59:20 +0900

    Update 1260.py

--- Baek_Joon/BFS/1260.py
+++ Baek_Joon/BFS/1260.py
@@ -1,4 +1,13 @@
+'''
+* DFS와 BFS
+  그래프를 DFS로 탐색한 결과와 BFS로 탐색한 결과를 출력하는 프로그램을 작성하시오. 
+  단, 방문할 수 있는 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문하고, 더 이상 방문할 수 있는 점이 없는 경우 종료한다. 
+  정점 번호는 1번부터 N번까지이다.
+'''
+
+
 from collections import deque
+import sys
 
 def dfs(graph, start, visited):
     visited[start]=True
@@ -20,10 +29,10 @@ def bfs(graph, start, visited):
                 visited[i]=True
         
 
-N,M,V = map(int, input().split())
+N,M,V = map(int, sys.stdin.readline().split())
 temp=[]
 for i in range(M):
-    temp.append(list(map(int,input().split())))
+    temp.append(list(map(int,sys.stdin.readline().split())))
     
 graph=[[] for _ in range(N+1)]
 for lst in temp:  # tree 형식으로 만들기
